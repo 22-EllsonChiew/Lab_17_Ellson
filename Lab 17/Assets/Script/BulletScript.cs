eb4877d748307eb91ce56a4eb2926e95e61e6736
Itzuzume <72852531+Itzuzume@users.noreply.github.com>
2021-01-18 11:38:02 +0800

    Lab17_Ellson

---
+++ Lab 17/Assets/Script/BulletScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BulletScript : MonoBehaviour
+{
+
+    public float speed;
+
+    public GameObject ScoreText;
+    private int Killcount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Killcount++;
+
+        ScoreText.GetComponent<Text>().text = "Killed: " + Killcount;
+    }
+}
---
+++ Lab 17/Assets/Script/Playercontroller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public float speed;
+    public float rotation;
+
+    public GameObject Bulletprefads;
+    public GameObject bulletSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalinput = Input.GetAxis("Vertical");
+        float horizontalinput = Input.GetAxis("Horizontal");
+
+
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            transform.Rotate(-Vector3.up * rotation * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+            transform.Rotate(Vector3.up * rotation * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Bulletprefads, bulletSpawn.transform.position, transform.rotation);
+        }
+    }
+}
